02972543261401b2e8eabc047f745a82f73028bb
Abdo5-Mahmoud <abdofwzy9@gmali.com>
2024-06-15 23:30:57 +0300

    update1

--- src/Component/Clock.jsx
+++ src/Component/Clock.jsx
@@ -3,26 +3,29 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 function Clock() {
-  let [clock, setclock] = useState("");
-  let thedate = () => {
-    const date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
+  // let [clock, setclock] = useState("");
+  // let thedate = () => {
+  //   const date = new Date();
+  //   let hours = date.getHours();
+  //   let minutes = date.getMinutes();
+  //   let seconds = date.getSeconds();
 
-    hours = hours < 10 ? "0" + hours : hours > 12 ? hours - 12 : hours;
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-    let currentTime = hours + ":" + minutes + ":" + seconds;
-    setclock(currentTime);
-  };
-
+  //   hours = hours < 10 ? "0" + hours : hours > 12 ? hours - 12 : hours;
+  //   minutes = minutes < 10 ? "0" + minutes : minutes;
+  //   seconds = seconds < 10 ? "0" + seconds : seconds;
+  //   let currentTime = hours + ":" + minutes + ":" + seconds;
+  //   setclock(currentTime);
+  // };
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
   useEffect(() => {
+    // setInterval(() => {
+    //   thedate();
+    // }, 1000);
     setInterval(() => {
-      thedate();
+      setTime(new Date().toLocaleTimeString());
     }, 1000);
   }, []);
-  return <div> {clock} </div>;
+  return <div> {time} </div>;
 }
 
 export default Clock;
